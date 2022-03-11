7092b176f0a1dc7b36530b3c56da787d94b52a18
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-03-11 09:40:19 -0600

    1st commit

---
+++ config.js
@@ -0,0 +1,8 @@
+// Step 1: declare mongoose
+const mongoose = require("mongoose");
+
+async function mongooseConnect() {
+  await mongoose.connect(process.env.MONGO_DB_URI);
+}
+
+module.exports = mongooseConnect;
---
+++ dummyData.js
@@ -0,0 +1,10 @@
+const ToDoData = [
+  {
+    title: "Review React",
+    textContent: "React, Store, Provider, THUNK, State, and PROPS",
+    created_at: "03/09/2022",
+    completed: false,
+  },
+];
+
+module.exports = ToDoData;
